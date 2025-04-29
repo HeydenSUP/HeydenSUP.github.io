8ef881897055e75f02202e9d5e8e130a82ee0002
heydenjx <s4074160@student.rmit.edu.au>
2025-04-29 19:51:07 +1000

    summary

--- assignment2/script.js
+++ assignment2/script.js
@@ -24,3 +24,74 @@ function updateProgressBar() {
   progressBar.style.width = value + "%";
 }
 // Add other functionalities here
+const speedBtn = document.querySelector("#speed-toggle-btn");
+console.log(speedBtn);
+if (speedBtn) {
+  let isFast = false;
+  //make clear it is fast or not
+  speedBtn.addEventListener("click", function () {
+    if (!video || !audio) return;
+
+    if (isFast) {
+      video.playbackRate = 1;
+      audio.playbackRate = 1;
+      speedBtn.textContent = "1x";
+    } else {
+      video.playbackRate = 2;
+      audio.playbackRate = 2;
+      speedBtn.textContent = "2x";
+    }
+    isFast = !isFast;
+  });
+}
+
+const muteBtn = document.querySelector("#mute-toggle-btn");
+console.log(muteBtn);
+const muteIcon = document.querySelector("#mute-icon");
+console.log(muteIcon);
+
+if (muteBtn) {
+  muteBtn.addEventListener("click", function () {
+    const newMutedState = !video.muted;
+
+    video.muted = newMutedState;
+    audio.muted = newMutedState;
+
+    if (newMutedState) {
+      muteIcon.src = "https://img.icons8.com/ios-glyphs/30/mute.png";
+      muteIcon.alt = "Muted";
+    } else {
+      muteIcon.src = "https://img.icons8.com/ios-glyphs/30/medium-volume.png";
+      muteIcon.alt = "Unmuted";
+    }
+  });
+}
+const fullscreenBtn = document.getElementById("fullscreen-btn");
+const fullScreenBtn = document.querySelector("#fullscreen-btn");
+console.log(fullScreenBtn);
+const fullscreenIcon = document.querySelector("#fullscreen-icon");
+console.log(fullscreenIcon);
+const playerContainer = document.querySelector(".media-player");
+console.log(playerContainer);
+
+if (fullscreenBtn && fullscreenIcon && playerContainer) {
+  fullscreenBtn.addEventListener("click", () => {
+    if (!document.fullscreenElement) {
+      playerContainer
+        .requestFullscreen()
+        .then(() => {
+          fullscreenIcon.src =
+            "https://img.icons8.com/ios-glyphs/30/collapse.png";
+          fullscreenIcon.alt = "Exit Fullscreen";
+        })
+        .catch((err) => {
+          console.error(`Fullscreen error: ${err.message}`);
+        });
+    } else {
+      document.exitFullscreen();
+      fullscreenIcon.src =
+        "https://img.icons8.com/ios-glyphs/30/full-screen--v1.png";
+      fullscreenIcon.alt = "Enter Fullscreen";
+    }
+  });
+}
